6e14199e97eea8d77cfbb09e033c6941541bbb52
Nicholas Zachary <nick@Nicholass-Air.hitronhub.home>
2019-01-04 20:22:06 -0800

    update comments

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN 
-  if (fromN > toN) {
+    // Sum all the values from fromN up to toN 
+    if (fromN > toN) {
       // base case
-      return 0
+          // run as much times until this is satisfied
+          // When this case is satisfied it will return this value to the last recursive case
+      return 2
   } else {
       // recursive case
+          // each time add by function value
+          // For the recursive function the value is +1
+          // For the base case it's the return value
       return fromN + sum(fromN + 1, toN)
   }
 }
